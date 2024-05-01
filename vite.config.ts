9384748416247ea403da982df03770763b5c5544
Nigel Ashiono <nigel.ashiono@sicon.co.uk>
2024-05-01 10:02:00 +0100

    feat: Modified config files to include components

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // or 'es5' or other target as needed
+    outDir: 'dist', // specify the output directory for the build
+    // other build options as needed
+  },
 })
